9db047d1f9e4ffeef3ba72599d8ff9e106d56641
Marco Veltman <marcovman@hotrmail.com>
2020-11-10 12:13:04 +0100

    Add Commands for button interaction

--- DNDinventory/ViewModel/MainViewModel.cs
+++ DNDinventory/ViewModel/MainViewModel.cs
@@ -1,14 +1,20 @@
-﻿using System;
+﻿using Prism.Commands;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace DNDinventory.ViewModel
 {
     class MainViewModel : INotifyPropertyChanged
     {
+        public MainViewModel()
+        {
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChange(string propertyName)

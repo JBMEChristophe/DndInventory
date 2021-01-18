deca67619b48dbf2f7fd5f97644c4b277bd06447
Toc532 <theocremers123@msn.com>
2021-01-18 17:45:27 +0100

    #14 implement feedback settingsfilehandler

--- DNDinventory/SettingsFileHandler.cs
+++ DNDinventory/SettingsFileHandler.cs
@@ -6,31 +6,43 @@ using System.Xml.Serialization;
 using System.IO;
 using System.Threading.Tasks;
 
-namespace DNDinventory {
-    public class SavedSettings {
+namespace DNDinventory 
+{
+    public class SavedSettings 
+    {
         // Set defaults here
         public string host = "localhost";
         public string port = "100";
         public string outputFolder = "Transfers";
     }
 
-    public class SettingsFileHandler {
-        public SavedSettings currentSettings = new SavedSettings();
-        XmlSerializer ser = new XmlSerializer(typeof(SavedSettings));
+    public class SettingsFileHandler 
+    {
+        public SavedSettings currentSettings;
+        XmlSerializer ser;
+
+        public SettingsFileHandler () 
+        {
+            currentSettings = new SavedSettings();
+            ser = new XmlSerializer(typeof(SavedSettings));
+        }
         
-        public void WriteToXml (string filename) {
-            TextWriter writer = new StreamWriter(filename);
-            ser.Serialize(writer, currentSettings);
-            writer.Close();
+        public void WriteToXml (string filename) 
+        {
+            using (var writer = new StreamWriter(filename))
+            {
+                ser.Serialize(writer, currentSettings);
+            }
         }
 
-        public void ReadFromXml (string filename) {
-            if (File.Exists(filename)) {
-                FileStream fs = new FileStream(filename, FileMode.Open);
-                try {
+        public void ReadFromXml (string filename)   
+        {
+            if (File.Exists(filename)) 
+            {
+                using (var fs = new FileStream(filename, FileMode.Open)) 
+                {
                     currentSettings = (SavedSettings) ser.Deserialize(fs);
-                } catch (Exception) { } //#TODO show 'file failed to load' error
-                fs.Close();
+                }
             }
         }
     }

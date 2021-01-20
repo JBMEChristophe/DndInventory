338db3988487afff5124672f25b430a851fa103b
Marco Veltman <marcovman@hotrmail.com>
2021-01-20 17:33:55 +0100

    #4 Add horizontal scrollable scroll viewer

---
+++ CustomControls/CustomScrollViewer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomControls
+{
+    /// <summary>
+    /// Interaction logic for CustomScrollViewer.xaml
+    /// </summary>
+    public partial class CustomScrollViewer : ScrollViewer
+    {
+        public CustomScrollViewer() : base()
+        {
+            InitializeComponent();
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            ScrollViewer scrollViewer = (ScrollViewer)sender;
+
+            if (scrollViewer == null)
+                return;
+
+            if (Keyboard.Modifiers != ModifierKeys.Shift)
+                return;
+
+            if (e.Delta < 0)
+            {
+                scrollViewer.LineRight();
+            }
+            else
+            {
+                scrollViewer.LineLeft();
+            }
+            e.Handled = true;
+        }
+    }
+}
